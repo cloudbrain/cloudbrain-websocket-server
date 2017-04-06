f68d5d9e88d960ff8d058312b80c1721493e9318
Marion Le Borgne <leborgne.mar@gmail.com>
2017-04-05 22:17:13 -0700

    refactoring: use --file  instead of --conf

---
+++ src/cbws/run.py
@@ -0,0 +1,91 @@
+import json
+import logging
+import time
+
+from argparse import ArgumentParser
+
+from cbws.server import WebsocketServer
+
+
+class _CommandLineArgError(Exception):
+    """ Error parsing command-line options """
+    pass
+
+
+class _Options(object):
+    """Options returned by _parseArgs"""
+
+    def __init__(self, conf_file, log_level):
+        """
+        :param config: (str) path to JSON config file.
+        :param log_level: (str) logger verbosity 'info' for logging.INFO 
+            or 'debug' for logging.DEBUG.
+        """
+        self.conf_file = conf_file
+
+        if log_level == 'info':
+            self.log_level = logging.INFO
+        elif log_level == 'debug':
+            self.log_level = logging.DEBUG
+
+
+def _parseArgs():
+    """
+    Parse command-line args
+    :rtype: _Options object
+    :raises _CommandLineArgError: on command-line arg error
+    """
+
+    parser = ArgumentParser(description="Start CloudBrain websocket server.")
+
+    parser.add_argument(
+        "--file",
+        type=str,
+        default=None,
+        dest="conf_file",
+        help="Path to JSON config file.")
+
+    parser.add_argument(
+        "--log",
+        type=str,
+        dest="log_level",
+        required=False,
+        default='info',
+        help="OPTIONAL: logger verbosity. Can be 'info' or 'debug'.")
+
+    options = parser.parse_args()
+
+    if options.conf_file is None:
+        raise ValueError("Module configuration must be provided with the "
+                         "--file flag.")
+
+    return _Options(conf_file=options.conf_file, log_level=options.log_level)
+
+
+def run(config, log_level):
+    logging.basicConfig(level=log_level)
+
+    with open(config, 'rb') as f:
+        config = json.load(f)
+        server = WebsocketServer(ws_server_port=config['ws_server_port'],
+                                 rabbitmq_address=config['rabbitmq_address'],
+                                 rabbitmq_user=config['rabbitmq_address'],
+                                 rabbitmq_pwd=config['rabbitmq_pwd'])
+        try:
+            server.start()
+            while 1:
+                time.sleep(0.1)
+        except KeyboardInterrupt:
+            server.stop()
+
+
+def main():
+    try:
+        options = _parseArgs()
+        run(options.conf_file, options.log_level)
+    except Exception as ex:
+        logging.exception("Wesocket server failed")
+
+
+if __name__ == '__main__':
+    main()
